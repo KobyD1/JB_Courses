3ec88cd2dac177bbc1f2e9961938f7a14ac97a36
KobyD1 <dkdk100@walla.co.il>
2023-02-15 20:29:17 +0200

    sdsss

---
+++ src/JavaTraining/MyFirstTest.java
@@ -0,0 +1,11 @@
+package JavaTraining;
+
+public class MyFirstTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Wolrd");
+        String firstName = "John";
+        int age = 35 ;
+        System.out.println("Hello Wolrd My name is "+firstName+" my age is "+age);
+    }
+
+}
