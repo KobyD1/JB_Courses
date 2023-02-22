03c7c384739399574a9140dbf5cd4882b7303ff7
KobyD1 <dkdk100@walla.co.il>
2023-02-22 21:15:34 +0200

    final

--- src/selenium/GoogleTest.java
+++ src/selenium/GoogleTest.java
@@ -1,14 +1,18 @@
 package selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
+import static java.lang.Thread.sleep;
+
 public class GoogleTest {
-    public static void main(String[] args) {
-        String url = "https://www.google.com/";
+    public static void main(String[] args) throws InterruptedException {
+        String url = "https://www.saucedemo.com/";
         System.out.println("Starting...");
         ChromeOptions options = new ChromeOptions();
 
@@ -18,6 +22,15 @@ public class GoogleTest {
         ChromeDriver driver = new ChromeDriver(options);
 
         driver.get(url);
+        WebElement user = driver.findElement(By.id("user-name"));
+        user.click();
+        user.sendKeys("standard_user");
+        WebElement pw = driver.findElement(By.id("password"));
+        pw.click();
+        pw.sendKeys("secret_sauce");
+        WebElement login  = driver.findElement(By.id("login-button"));
+        login.click();
+        sleep(3000);
         driver.close();
     }
 }
